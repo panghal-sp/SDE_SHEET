a04e224c1fbe6f78e0e853cc8ec9bdb3f3a2ab46
Sahil Panghal <103810118+panghal-sp@users.noreply.github.com>
2022-08-19 11:21:46 +0530

    Create ZigZagConversion.java
    Solution for leetcode ZigZagConversion

---
+++ ZigZagConversion.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class Solution {
+StringBuffer ans=new StringBuffer("");
+    public static void main(String [] args){
+        System.out.println("Started");
+        new Solution().convert("PAYPALISHIRING",4);
+    }
+
+
+        public String convert(String s, int numRows) {
+
+        if(numRows==1){
+            ans.append(s);
+        } else if (numRows==2) {
+            StringBuffer one =new StringBuffer();
+            StringBuffer two =new StringBuffer();
+            for(int i=0;i<s.length();i++){
+                if(i==0 || i%2==0)
+                    one.append(s.charAt(i));
+                else if (i==1 || i%2 !=0)
+                    two.append(s.charAt(i));
+
+            }
+            ans.append(one).append(two);
+        } else if (numRows>2) {
+
+            char str []=s.toCharArray();
+            String arr [] = new String[numRows];
+            Arrays.fill(arr,"");
+            int row=0;
+            boolean down=true;
+            for(int i=0; i<s.length();i++){
+                arr[row] += (str[i]);
+
+                if(row==0){
+                   down=true;
+                }else if(row==numRows-1)
+                    down=false;
+
+                if(down)
+                    row++;
+                else row--;
+
+            }
+           ans.append(Arrays.stream(arr).reduce((j,k)->j+k).get());
+        }
+
+            System.out.println(ans);
+            return ans.toString();
+        }
+
+}
